d31de8852ecc8209b790cf82e76a7e6adbc7fd6f
ristoxxx <lahteri@gmail.com>
2020-09-19 18:50:16 +0300

    Redux vaiheessa vielä

---
+++ Redux/Reduxperiaatteet.js
@@ -0,0 +1,62 @@
+/*Tässä tehtävässä on tarkoitus rakentaa pieni scripti 
+värien lisäämiseen ja poistamiseen säiliöstä Reduxin periaatteiden mukaisesti. 
+Lopulta scriptin pitäisi siis tulosta "keltainen", "vihreä"
+Muista ajaa ensin src-kansiossa komento "npm install"
+Voit ajaa tämän tiedoston komennolla "node Reduxperiaatteet.js" tai "npm run build"
+*/
+
+//import Redux from 'redux' //ES6 syntaksilla voisi tehdä näin
+
+const Redux = require('redux')
+
+//Action 1
+function addColor(value){
+    return{
+        type: "ADD", //FIXME X
+        color: value //FIXME X
+    };
+}
+
+//Action 2
+function removeColor(value){
+    return{
+        type: "REMOVE", //FIXME X
+        color: value //FIXME X
+    };
+}
+
+/*
+Pohja reducerille, ota edellinen tila ja Action ja palauta uusi tila.
+Tämä reducer-funktio on siis "idealtaan samantapainen" kuin funktionaalisen ohjelmoinnin 
+reduce-funktio, eli Array.prototype.reduce(reducer, ?initialValue)
+*/
+function favoriteColors(state, action){ 
+    //console.log(state);
+    if(state == undefined){
+        state = []
+    }
+
+    if(action.type === "ADD"){
+        console.log(action.color, state);
+        return state.concat(action.color);
+    }else if(action.type === "REMOVE"){
+        console.log(action.color, action)
+        return state.filter(function(item){ 
+            //selvitä tarvittaessa miten toimii funktionaalisen ohjelmoinnin filter-funktio
+            return state //FIXME X
+        });
+    }else{
+        return state;
+    }
+}
+
+//Luodaan redux-säiliö ja käytetään sitä
+let store = Redux.createStore(favoriteColors);
+store.dispatch(addColor("sininen"));
+store.dispatch(addColor("keltainen"));
+store.dispatch(addColor("vihreä"));
+store.dispatch(removeColor("sininen"));
+
+console.log(store.getState()); //pitäisi tulla tuloste ['keltainen', 'vihreä']
+var c = "moi";
+//console.log(c);
