6cb6d68976378169f09adebe7d41766973c7acbc
Risto Lähteenkorva <56062215+ristoxxx@users.noreply.github.com>
2020-10-08 09:57:27 +0300

    Update test_postinumerot.py

--- Postinumerot/test_postinumerot.py
+++ Postinumerot/test_postinumerot.py
@@ -17,7 +17,7 @@ data = {
     "32201": "LOIMAA"
 }
 
-"""testaa pienellä kirjoitetun nimen"""
+"""testaa pienella kirjoitetun nimen"""
 def test_hae_postinumerot_somero():    
     kaupunki = 'somero'
     lista = hae_postinumerot(data, kaupunki)
@@ -29,25 +29,25 @@ def test_hae_postinumerot_SOMERO():
     lista = hae_postinumerot(data, kaupunki)
     assert lista == ['31401, ', '31400']
 
-"""testaa jos Ensimmäinen kirjain isolla"""
+"""testaa jos Ensimmainen kirjain isolla"""
 def test_hae_postinumerot_Somero():
     kaupunki = 'Somero'
     lista = hae_postinumerot(data, kaupunki)
     assert lista == ['31401, ', '31400']
 
-"""testaa palauttaako tyhjä syöte tyhjän listan"""
+"""testaa palauttaako tyhja syote tyhjan listan"""
 def test_hae_postinumerot_if_empty():
     kaupunki = ''
     lista = hae_postinumerot(data, kaupunki)
     assert lista == ['']
 
-"""testaa mitä tapahtuu jos syöte ei löydy listalta"""
+"""testaa mita tapahtuu jos syote ei loydy listalta"""
 def test_hae_postinumerot_if_not_on_list():
     kaupunki = 'lvnslrvjr'
     lista = hae_postinumerot(data, kaupunki)
     assert lista == ['']
 
-"""testaa mitä tapahtuu jos syötetään numero kun pyydetään nimeä"""
+"""testaa mita tapahtuu jos syotetaan numero kun pyydetaan nimea"""
 def test_hae_postinumerot_if_number():
     kaupunki = '00100'
     lista = hae_postinumerot(data, kaupunki)
@@ -59,19 +59,19 @@ def test_hae_kaupunki_loimaa():
     kaupunki = hae_kaupunki(data, numero)
     assert kaupunki == 'LOIMAA'
 
-"""testaa kaupungin hakeminen jos tyhjä syöte"""
+"""testaa kaupungin hakeminen jos tyhja syote"""
 def test_hae_kaupunki_if_empty():
     numero = ''
     kaupunki = hae_kaupunki(data, numero)
     assert kaupunki == ''
     
-"""testaa kaupungin hakeminen jos numeroa ei löydy"""    
+"""testaa kaupungin hakeminen jos numeroa ei loydy"""    
 def test_hae_kaupunki_if_not_found():
     numero = '69696'
     kaupunki = hae_kaupunki(data, numero)
     assert kaupunki == ''
 
-"""testaa kaupungin hakeminen jos syötetään nimi kun pitäisi syöttää numero"""    
+"""testaa kaupungin hakeminen jos syotetaan nimi kun pitaisi syottaa numero"""    
 def test_hae_kaupunki_if_text():
     numero = 'LOIMAA'
     kaupunki = hae_kaupunki(data, numero)
